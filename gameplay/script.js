0ba81e325bdff90b66c83304e3a997ff38c5ca17
tim25871014 <tim25871014@gmail.com>
2026-05-30 17:33:36 +0800

    修整廣告看板

--- gameplay/script.js
+++ gameplay/script.js
@@ -131,42 +131,57 @@ const adInfo = createApp({ setup() {
     const adIndex = ref(0);
     const currentAdUrl = ref('');
     const adOpacity = ref(1);
+    const showAd = computed(() => adImages.value.length > 0);
     let adTimer = null;
     let fadeTimer = null;
 
+    const setAd = (index) => {
+        adIndex.value = index;
+        currentAdUrl.value = adImages.value[index] || '';
+        adOpacity.value = 1;
+    };
+
+    const clearTimers = () => {
+        if (adTimer) clearInterval(adTimer);
+        if (fadeTimer) clearTimeout(fadeTimer);
+        adTimer = null;
+        fadeTimer = null;
+    };
+
     const swapAd = () => {
-        if (!adImages.value.length) return currentAdUrl.value = '', adOpacity.value = 1;
+        if (!adImages.value.length) return setAd(0);
         adOpacity.value = 0;
         if (fadeTimer) clearTimeout(fadeTimer);
         fadeTimer = setTimeout(() => {
-            adIndex.value = (adIndex.value + 1) % adImages.value.length;
-            currentAdUrl.value = adImages.value[adIndex.value] || '';
-            adOpacity.value = 1;
+            setAd((adIndex.value + 1) % adImages.value.length);
         }, 250);
     };
 
     const startRotation = () => {
-        if (adTimer) clearInterval(adTimer);
-        if (!adImages.value.length) return currentAdUrl.value = '', adOpacity.value = 1;
-        currentAdUrl.value = adImages.value[adIndex.value] || '';
-        adOpacity.value = 1;
-        if (adImages.value.length > 1) {
-            adTimer = setInterval(() => { swapAd(); }, 20000);
-        }
+        clearTimers();
+        if (!adImages.value.length) return setAd(0);
+        setAd(adIndex.value);
+        if (adImages.value.length > 1) adTimer = setInterval(swapAd, 20000);
     };
 
-    fetch('../_data/img/ad/ad-list.json').then(res => res.json()).then(list => {
-            adImages.value = Array.isArray(list) ? list.map(file => `../_data/img/ad/${file}`) : [];
-            startRotation();
-        }).catch(() => {
-            adImages.value = [];
-            startRotation();
-        });
-
-    return {
-        currentAdUrl,
-        adOpacity
+    const preloadAds = (files) => {
+        if (!Array.isArray(files) || files.length === 0) return Promise.resolve([]);
+        const urls = files.map(file => `../_data/img/ad/${file}`);
+        return Promise.all(urls.map((url) => new Promise((resolve) => {
+            const img = new Image();
+            img.onload = () => resolve(url);
+            img.onerror = () => resolve(null);
+            img.src = url;
+        }))).then(results => results.filter(Boolean));
     };
+
+    fetch('../_data/img/ad/ad-list.json')
+        .then(res => res.json()).then(preloadAds).then(validUrls => {
+            adImages.value = validUrls;
+            startRotation(); })
+        .catch(() => { adImages.value = []; startRotation(); });
+
+    return { currentAdUrl, adOpacity, showAd };
 }}).mount('#advertisment');
 
 /////////////////////////////////////////////////////////////
@@ -269,7 +284,10 @@ function updateScoreData(tourneyMng) {
     scoreInfo.RedScore = redTeamScore;
     scoreInfo.BlueScore = blueTeamScore;
     scoreInfo.BarWidth = mapTanh(redTeamScore - blueTeamScore);
-    scoreInfo.BorderWidth = (!controlPanel.comboMode) ? 3 * mapTanh(redTeamComboScore - blueTeamComboScore) : 0;
+    scoreInfo.BorderWidth = (!controlPanel.comboMode) ?
+        3 * mapTanh(redTeamComboScore - blueTeamComboScore) :
+        5 * Math.sign(mapTanh(redTeamScore - blueTeamScore))
+    ;
 }
 
 function mapTanh(x) {
